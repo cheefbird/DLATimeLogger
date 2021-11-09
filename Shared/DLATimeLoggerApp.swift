07241c60118301cc73443d82b241aff387d9037b
Francis Breidenbach <fbreidenbach@gmail.com>
2021-11-09 11:36:28 -0800

    Initial Commit

---
+++ Shared/DLATimeLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DLATimeLoggerApp.swift
+//  Shared
+//
+//  Created by Francis Breidenbach on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct DLATimeLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
