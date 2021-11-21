3ef7198c8380a7b3b90b37b3a707a6900cfaab52
Francis Breidenbach <fbreidenbach@gmail.com>
2021-11-20 19:46:35 -0800

    Create CalendarCell

---
+++ Shared/Views/CalendarCell.swift
@@ -0,0 +1,37 @@
+//
+//  CalendarStrip.swift
+//  DLATimeLogger
+//
+//  Created by Francis Breidenbach on 11/20/21.
+//
+
+import SwiftUI
+
+struct CalendarCell: View {
+  var day: String
+  var hours: Double
+  
+  var body: some View {
+    VStack(alignment: .center) {
+      Text("M")
+        .font(.body)
+        .bold()
+        .foregroundColor(.gray)
+        .padding(.bottom, 2)
+      
+      Text(String(hours))
+        .font(.caption)
+        .foregroundColor(.gray)
+    }
+    .frame(width: 40, height: 50)
+    .background(Color.black)
+  }
+}
+
+struct CalendarStrip_Previews: PreviewProvider {
+  static var previews: some View {
+    CalendarCell(
+      day: "M", hours: 2.67
+    )
+  }
+}
